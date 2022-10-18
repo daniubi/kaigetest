b96636e8a3a6c78c62b1741163d832ada3f9d8d3
张凯 <804703017@qq.com>
2022-10-18 15:14:32 +0800

    Initial commit

---
+++ springboot-spring/src/main/java/com/aikaige/springbootspring/SpringbootSpringApplication.java
@@ -0,0 +1,13 @@
+package com.aikaige.springbootspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpringApplication.class, args);
+    }
+
+}
